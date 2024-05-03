8e1013273d6f2c1965eec0b8b2ceadd6a9fff683
Pedro Nunes <nunespedronunes@yahoo.com.br>
2024-05-03 15:04:28 -0300

    req 2 bem parecido do 1, assim a gente gosta

--- news/models.py
+++ news/models.py
@@ -9,3 +9,13 @@ class Category(models.Model):
     # aqui antes copilot colocou um campo de descrição, mas não é necessário
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
