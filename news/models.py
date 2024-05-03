62c6925b69a919177a23935bc796fd337b062598
Pedro Nunes <nunespedronunes@yahoo.com.br>
2024-05-03 18:07:20 -0300

    req 3 relendo conteudo e grav aula

--- news/models.py
+++ news/models.py
@@ -1,6 +1,8 @@
 # req 1 por aqui aparentemente
 from django.db import models
 from news.validator import validate_even
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -19,3 +21,38 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_title(value):
+    if len(value.split()) <= 1:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[validate_title],  # aqui validação personalizada
+        null=False,
+        blank=False,
+    )
+    content = models.TextField(null=False, blank=False)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="news",  # aqui é o relacionamento com o modelo User
+        null=False,
+    )
+    created_at = models.DateField(
+        default=timezone.now, null=False, blank=False
+    )
+    image = models.ImageField(
+        upload_to="img/", null=True, blank=True  # aqui localização do upload
+    )
+    categories = models.ManyToManyField(
+        "Category",  # aqui aponta para o modelo Category
+        related_name="news",  # aqui é o relacionamento inverso
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.title
