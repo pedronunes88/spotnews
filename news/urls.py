33cd0129bb0a95dc3718cd9f7f47fe84a5ba9741
Pedro Nunes <nunespedronunes@yahoo.com.br>
2024-05-09 22:04:33 -0300

    req 5 foi mais facil depois de entender os erros do req 4, porem to cansado demais

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import news_home
+from .views import news_home, news_details
 
 urlpatterns = [
     path("", news_home, name="home-page"),
+    path("news/<int:id>/", news_details, name="news-details-page"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
 
 # Create your views here.
@@ -7,3 +7,8 @@ from .models import News
 def news_home(request):
     news = News.objects.all()
     return render(request, "home.html", {"news": news})
+
+
+def news_details(request, id):
+    news = get_object_or_404(News, id=id)
+    return render(request, "news_details.html", {"news": news})
